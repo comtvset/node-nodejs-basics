75a0fb47e208bbcf2a3809220c112b34d5d23780
comtvset <comtvset@gmail.com>
2025-01-24 12:45:35 +0100

    feat: module-hash is done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { join } from 'node:path';
+import { pipeline } from 'stream/promises';
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+
+const currentDir = import.meta.dirname;
+const targetDir = join(currentDir, 'files');
+const file = 'fileToCalculateHashFor.txt';
+
+const filePath = join(targetDir, file);
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const hash = createHash('sha256');
+    await pipeline(createReadStream(filePath), hash);
+    console.log(`Hash for file "${file}": ${hash.digest('hex')}`);
+  } catch (err) {
+    console.error('Hash calculating failed :(');
+  }
 };
 
 await calculateHash();
