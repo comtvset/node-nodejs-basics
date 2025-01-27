302159ff7baf2eaeae46f83149c1c2c2a1d00b18
Vadzim <49336945+comtvset@users.noreply.github.com>
2025-01-27 13:40:35 +0100

    feat: module-streams is done

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,29 @@
+import { join } from 'node:path';
+import { createReadStream } from 'fs';
+import { stdout } from 'node:process';
+
+const currentDir = import.meta.dirname;
+const targetDir = join(currentDir, 'files');
+const file = 'fileToRead.txt';
+
+const filePath = join(targetDir, file);
+
 const read = async () => {
-    // Write your code here 
+  stdout.write(
+    `\n\n\n\n\x1b[36m⋇⋇⋇ Welcome to "Reader"! ⋇⋇⋇\n\n\x1b[37m`
+  );
+
+  const stream = createReadStream(filePath, 'utf-8');
+  stream.on('data', (chunk) => stdout.write(chunk + '\n'));
+
+  stream.on('error', (err) => {
+    console.error('\x1b[31m>>> Failed to read file >>>\x1b[0m', err);
+    stream.destroy();
+  });
+
+  stream.on('end', () => {
+    console.log('\n\x1b[32mFile read successfully!');
+  });
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,31 @@
+import { join } from 'node:path';
+import { createWriteStream } from 'fs';
+import { stdin, stdout } from 'node:process';
+
+const currentDir = import.meta.dirname;
+const targetDir = join(currentDir, 'files');
+const file = 'fileToWrite.txt';
+
+const filePath = join(targetDir, file);
+
 const write = async () => {
-    // Write your code here 
+  const stream = createWriteStream(filePath, 'utf-8');
+
+  stream.on('error', (err) => {
+    console.error('\x1b[31m>>> Something went wrong >>>\x1b[0m', err);
+    stream.destroy();
+  });
+
+  stdin.pipe(stream);
+  stdout.write(
+    `\n\n\n\n\x1b[36m⋇⋇⋇ Welcome to "Writer"! ⋇⋇⋇\n\x1b[31m(note: to terminate the program, press "ctrl + c")\x1b[33m\n \n⤥ Please write something below ⤦\n\n\x1b[37m`
+  );
+
+  process.on('SIGINT', () => {
+    stdout.write(`\n\n\n\x1b[32mFile "${file}" written successfully!`);
+    stdout.write('\n\x1b[33mSee you soon...');
+    process.exit();
+  });
 };
 
 await write();
