4d2bbc3bb0af72dfe8f2db12bf11866e0d848d6d
comtvset <comtvset@gmail.com>
2025-01-27 15:52:55 +0100

    feat: module-zip is done

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,33 @@
+import { join } from 'node:path';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import fs from 'fs';
+
 const compress = async () => {
-    // Write your code here 
+  const currentDir = import.meta.dirname;
+  const targetDir = join(currentDir, 'files');
+  const file = 'fileToCompress.txt';
+
+  const filePathFrom = join(targetDir, file);
+  const source = createReadStream(filePathFrom);
+
+  const compressFile = `${file}.gz`;
+  const filePathTo = join(targetDir, compressFile);
+  const destination = createWriteStream(filePathTo);
+
+  const remove = join(targetDir, compressFile);
+  const gzip = createGzip();
+
+  pipeline(source, gzip, destination, (err) => {
+    if (err) {
+      fs.unlink(remove, () => {});
+      console.error('\x1b[31m>>> Failed to compress >>>\x1b[0m', err);
+      process.exitCode = 1;
+    } else {
+      console.log('\n\x1b[32mCompression successful!');
+    }
+  });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,33 @@
+import { join } from 'node:path';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import fs from 'fs';
+
 const decompress = async () => {
-    // Write your code here 
+  const currentDir = import.meta.dirname;
+  const targetDir = join(currentDir, 'files');
+  const file = 'fileToCompress.txt.gz';
+
+  const filePathFrom = join(targetDir, file);
+  const source = createReadStream(filePathFrom);
+
+  const compressFile = `${file.slice(0, -3)}`;
+  const filePathTo = join(targetDir, compressFile);
+  const destination = createWriteStream(filePathTo);
+
+  const remove = join(targetDir, compressFile);
+  const unzip = createGunzip();
+
+  pipeline(source, unzip, destination, (err) => {
+    if (err) {
+      fs.unlink(remove, () => {});
+      console.error('\x1b[31m>>> Failed to decompress >>>\x1b[0m', err);
+      process.exitCode = 1;
+    } else {
+      console.log('\n\x1b[32mDecompression successful!');
+    }
+  });
 };
 
 await decompress();
