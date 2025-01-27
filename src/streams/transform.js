889b8d41025747a4b418166e5ce51e8fd7f2e38f
comtvset <comtvset@gmail.com>
2025-01-27 13:29:47 +0100

    feat: module-streams is done

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,28 @@
+import { stdout, stdin } from 'node:process';
+import { Transform } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+  stdout.write(
+    `\n\n\n\n\x1b[36m⋇⋇⋇ Welcome to "Transformer"! ⋇⋇⋇\n\x1b[31m(note: to terminate the program, press "ctrl + c")\x1b[33m\n \n⤥ Please write something below ⤦\n\n\x1b[37m`
+  );
+
+  const reverse = new Transform({
+    transform(chunk, encoding, callback) {
+      let input = chunk.toString().trim();
+      let reverse = input.split('').reverse().join('');
+
+      this.push(`\x1b[34m${input}  \x1b[0m>  \x1b[32m${reverse}\n\x1b[0m`);
+
+      callback();
+    },
+  });
+
+  stdin.pipe(reverse).pipe(stdout);
+
+  process.on('SIGINT', () => {
+    stdout.write('\n\n\n\x1b[33mSee you soon...');
+    process.exit();
+  });
 };
 
 await transform();
