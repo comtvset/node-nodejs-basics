f94530578d879a0e3a5c48441394ae7345a25d52
comtvset <comtvset@gmail.com>
2025-01-29 13:05:16 +0100

    feat: module-cp is done

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,14 @@
+import { join } from 'node:path';
+import { fork } from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const filePath = join(import.meta.dirname, 'files', 'script.js');
+
+  const forked = fork(filePath, args);
+
+  forked.on('close', (code) => {
+    console.log(code);
+  });
 };
 
 // Put your arguments in function call to test this functionality
