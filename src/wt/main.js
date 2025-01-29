39a99681de9f36945c84cace64fe632fec238249
Vadzim <49336945+comtvset@users.noreply.github.com>
2025-01-29 10:41:50 +0100

    feat: module-wt is done

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,32 @@
+import { join } from 'node:path';
+import { Worker } from 'node:worker_threads';
+import os from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+  const currentDir = import.meta.dirname;
+  const workerFile = join(currentDir, 'worker.js');
+  const numCPUs = os.cpus().length;
+  const promises = [];
+
+  for (let i = 0; i < numCPUs; i++) {
+    const worker = new Worker(workerFile, { workerData: 10 + i });
+
+    const workerPromise = new Promise((resolve) => {
+      worker.on('message', (message) => {
+        resolve({ status: 'resolved', data: message.message });
+      });
+
+      worker.on('error', (err) => {
+        console.error(err);
+        resolve({ status: 'error', data: null });
+      });
+    });
+
+    promises.push(workerPromise);
+  }
+
+  const results = await Promise.all(promises);
+  console.log(results);
 };
 
 await performCalculations();
